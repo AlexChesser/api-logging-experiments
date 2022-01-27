c1f9323ed9202dafb92c1a3095bc5cfa27c37f72
Alexander Chesser <alex.chesser@zensurance.com>
2022-01-27 10:22:31 -0500

    fixed import error

--- api/src/controllers/invoice.controller.ts
+++ api/src/controllers/invoice.controller.ts
@@ -1,12 +1,13 @@
-import { Router,  Response, NextFunction } from "express"
+import { Router,  Request, Response, NextFunction  } from "express"
 import { LoggerStart, LoggerEnd } from "../middleware/logging.middleware";
 
 const router = Router()
 
 async function stub(req: Request, res: Response, next: NextFunction): Promise<void> {
     console.log("stub", req.url)
-    res.json({ message: "ok" });
+    res.status(200).json({ message: "ok" });
     next();
+    return;
 }
 
 router.get("/invoice", LoggerStart, stub, LoggerEnd);
