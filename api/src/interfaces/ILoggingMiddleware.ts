f696fcf06ecbb477e9e7ff5fb26ede39145bed8e
Alexander Chesser <alex.chesser@gmail.com>
2022-01-28 20:30:21 -0500

    eod

--- api/src/interfaces/ILoggingMiddleware.ts
+++ api/src/interfaces/ILoggingMiddleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express"
 
-
 export interface ILoggingMiddleware {
     Start(req: Request, res: Response, next: NextFunction) : void;
     End(req: Request, res: Response, next: NextFunction) : void;
